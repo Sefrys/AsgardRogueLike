ec74a20915ac0cc9841f7aefe79f13ace4ad31e2
Irek Zagan <sefrys@gmail.com>
2017-08-24 18:14:12 +0200

    Add character name and class choosing in char. creator

--- RogueLike.py
+++ RogueLike.py
@@ -1,4 +1,4 @@
-
+import os
 
 # Print story of game, wait for enter input/wait some time
 # to call display_menu_screen.
@@ -16,7 +16,7 @@ def menu_display_how_to_play():
     pass
 
 
-# highscores, records
+# highscores, records --- Irek
 def menu_display_hall_of_fame():
     pass
 
@@ -31,21 +31,64 @@ def menu_exit_game():
     pass
 
 
+# Irek
 # Initiates new game sequence; character creation
 def menu_new_game():
-    pass
+    os.system("clear")
+    create_character()
 
 
+# Irek
 # Choose name, class, print attribute table, assign new
 # attributes to character. Option to reset atr. to deafult.
 def create_character():
-    pass
+    list_of_classes = ["warrior", "mage", "rogue"]
+
+    ask_name = "Welcome!" + "\n" + "What is your name, traveler?" + "\n"
+    for i in ask_name:
+        sys.stdout.write(i)
+        sys.stdout.flush()
+        time.sleep(.05)
+
+    while True:
+        player_name = input("My name is: ").title()
+        if not all(x.isalpha() for x in player_name):
+            answer_invalid_name = "I'm sorry, you have to tell me your true name."
+            for i in answer_wrong_name:
+                sys.stdout.write(i)
+                sys.stdout.flush()
+                time.sleep(.05)
+        else:
+            answer_valid_name = ("Very well, nice to meet you, " + player_name + "." + "\n"
+                                 + "Now, are you a warrior, a mage or a rogue?" + "\n")
+            for i in answer_valid_name:
+                sys.stdout.write(i)
+                sys.stdout.flush()
+                time.sleep(.05)
+            break
+
+    while True:
+        player_class = input("I am a: ").lower()
+        if player_class not in list_of_classes:
+            answer_invalid_class = "Surely you jest, you must be either a warrior, a mage or a rogue!"
+            for i in answer_invalid_class:
+                sys.stdout.write(i)
+                sys.stdout.flush()
+                time.sleep(.05)
+        else:
+            answer_valid_class = ("A " + player_class + "? Good. Let's see what you are capable of then!" + "\n")
+            for i in answer_valid_class:
+                sys.stdout.write(i)
+                sys.stdout.flush()
+                time.sleep(.05)
+            break
+
+
 
 
 # initiates game fucntions
 def game_core():
-    display_intro_screen()
-    display_menu_screen()
+    create_character()
 
 
 def main():
