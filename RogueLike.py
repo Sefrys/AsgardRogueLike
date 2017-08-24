2357b54772f033a167fade5beb6c679e1ddfcfa7
Irek Zagan <sefrys@gmail.com>
2017-08-24 14:47:00 +0200

    Add game outline

---
+++ RogueLike.py
@@ -0,0 +1,55 @@
+
+
+# Print story of game, wait for enter input/wait some time
+# to call display_menu_screen.
+def display_intro_screen():
+    pass
+
+
+# Display menu choices
+def display_menu_screen():
+    pass
+
+
+# Info about WSAD movement, attributes and dice combat
+def menu_display_how_to_play():
+    pass
+
+
+# highscores, records
+def menu_display_hall_of_fame():
+    pass
+
+
+# What the game is about, credits
+def menu_display_about_screen():
+    pass
+
+
+# Exits the program, ask Y/N
+def menu_exit_game():
+    pass
+
+
+# Initiates new game sequence; character creation
+def menu_new_game():
+    pass
+
+
+# Choose name, class, print attribute table, assign new
+# attributes to character. Option to reset atr. to deafult.
+def create_character():
+    pass
+
+
+# initiates game fucntions
+def game_core():
+    display_intro_screen()
+    display_menu_screen()
+
+
+def main():
+    game_core()
+
+
+main()
