2aa7cdec9f108327af1a7d2da32ca6bb271368ec
filipHartman <filip.hartman23@gmail.com>
2017-08-25 11:40:50 +0200

    Add file with function dispalying menu of the game

---
+++ menu.py
@@ -0,0 +1,27 @@
+def display_menu_screen():
+    indent = 5
+    offset = 2
+#to extract to external file
+    menu_options = {1: "Start game",
+                    2: "How-to-play",
+                    3: "Hall of Fame",
+                    4: "About screen",
+                    5: "Exit game"}
+    key_lenght = 1
+    value_lenght = 0
+    for i in menu_options.values():
+        if int(len(i)) > value_lenght:
+            value_lenght = int(len(i))
+
+    dash_bar = key_lenght + value_lenght + offset
+
+    print("{:>{width}}{:}".format(
+        "_"*dash_bar, "\n", width=(indent+value_lenght + offset)))
+    for i in menu_options:
+        print("{:>{width}}{:{w}}{:<}".format(
+                i, ".", menu_options[i], width=indent, w=offset))
+    print("{:>{width}}".format(
+        "_"*dash_bar, width=(indent+value_lenght + offset)))
+
+
+display_menu_screen()
