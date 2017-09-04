e9625ff161a020712e75fbfc0cdb324852a55426
Irek Zagan <sefrys@gmail.com>
2017-09-04 16:22:27 +0200

    add introScreen printing

---
+++ introScreen.py
@@ -0,0 +1,16 @@
+import sys
+import time
+
+def slow_print(string, delay=.001):
+    for i in string:
+        sys.stdout.write(i)
+        sys.stdout.flush()
+        time.sleep(delay)
+
+def introduction_title():
+    ''' prints game title in ascii '''
+    game_title = open('gameTitle.txt', "r")
+    print_game_name = game_title.read()
+    slow_print(print_game_name)
+    game_title.close()
+    time.sleep(1)
