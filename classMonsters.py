aa74b848a1553cc446c6755e530585d8ad3805e5
Irek Zagan <sefrys@gmail.com>
2017-09-07 11:21:28 +0200

    Modified wolf stats to be lower

--- classMonsters.py
+++ classMonsters.py
@@ -1,5 +1,5 @@
 cave_bat = {'INT': 2, 'STA': 5, 'DEX': 5, 'STR': 6}
 cave_bat_name = "Cave Bat"
 
-wolf = {'INT': 3, 'STA': 12, 'DEX': 9, 'STR': 10}
-wolf_name = "Direwolf"
+wolf = {'INT': 3, 'STA': 11, 'DEX': 8, 'STR': 7}
+wolf_name = "Wolf"
